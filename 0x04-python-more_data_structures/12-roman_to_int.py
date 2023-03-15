1c813b9ab3be6ddf0ab045607669b97614e83857
gontsenaome <gontsenaome243@gmail.com>
2023-03-15 04:34:31 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def to_subtract(list_num):
         if max_list > n:
             to_sub += n
 
-    return (max_list - to_sub)
+            return (max_list - to_sub)
 
 def roman_to_int(roman_string):
     if not roman_string:
@@ -26,14 +26,12 @@ def roman_to_int(roman_string):
     for ch in roman_string:
         for r_num in list_keys:
             if r_num == ch:
-                 if rom_n.get(ch) <= last_rom:
-                    num += to_subtract(list_num)
-                    list_num = [rom_n.get(ch)]
+                if rom_n.get(ch) <= last_rom:
+                     num += to_subtract(list_num)
+                     list_num = [rom_n.get(ch)]
                 else:
-                    list_num.append(rom_n.get(ch))
+                    list_num.append(rom_n.get(ch)last_rom = rom_n.get(ch)
 
-                last_rom = rom_n.get(ch)
-
-    num += to_subtract(list_num)
+                            num += to_subtract(list_num)
 
     return (num)
